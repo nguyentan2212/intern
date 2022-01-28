87bc770dd6dbecee49bc15325fdbb8cac0664413
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2022-01-28 09:23:09 +0700

    add signup route

--- molaris-project/src/App.js
+++ molaris-project/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./views/auth/SignUp";
 import Dashboard from "./views/dashboard/Dashboard";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/auth/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </div>
