25fec41163ce240b7a8b84a54d23057c9f97c832
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2022-01-28 09:23:27 +0700

    add moralis provider

--- molaris-project/src/index.js
+++ molaris-project/src/index.js
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import App from "./App";
+import { MoralisProvider } from "react-moralis";
 
 const mdTheme = createTheme();
+const serverUrl = process.env.REACT_APP_SERVER_URL;
+const appId = process.env.REACT_APP_ID;
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={mdTheme}>
-      <App />
-    </ThemeProvider>
+    <MoralisProvider appId={appId} serverUrl={serverUrl}>
+      <ThemeProvider theme={mdTheme}>
+        <App />
+      </ThemeProvider>
+    </MoralisProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
