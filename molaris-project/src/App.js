85373b567f6d534bbb54e0cf7421a96612fba698
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2022-01-28 10:16:32 +0700

    signin route

--- molaris-project/src/App.js
+++ molaris-project/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./views/auth/SignIn";
 import SignUp from "./views/auth/SignUp";
 import Dashboard from "./views/dashboard/Dashboard";
 
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/auth/signup" element={<SignUp />} />
+          <Route path="/auth/signin" element={<SignIn />} />
         </Routes>
       </BrowserRouter>
     </div>
