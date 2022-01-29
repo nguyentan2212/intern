3209f08a48fc0b34e68476eead30f59f0ad2a26d
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2022-01-29 11:35:50 +0700

    add reset password route

--- molaris-project/src/App.js
+++ molaris-project/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ResetPassword from "./views/auth/ResetPassword";
 import SignIn from "./views/auth/SignIn";
 import SignUp from "./views/auth/SignUp";
 import Dashboard from "./views/dashboard/Dashboard";
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/auth/signup" element={<SignUp />} />
           <Route path="/auth/signin" element={<SignIn />} />
+          <Route path="/auth/resetpassword" element={<ResetPassword />} />
         </Routes>
       </BrowserRouter>
     </div>
