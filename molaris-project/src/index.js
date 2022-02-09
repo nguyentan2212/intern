dc1f5b0a46cd59cf9aa51a4a30940c80136abcd3
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2022-02-09 09:36:47 +0700

    material ui for sweetalert2

--- molaris-project/src/index.js
+++ molaris-project/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import App from "./App";
 import { MoralisProvider } from "react-moralis";
+import "@sweetalert2/theme-material-ui/material-ui.css";
 
 const mdTheme = createTheme();
 const serverUrl = process.env.REACT_APP_SERVER_URL;
