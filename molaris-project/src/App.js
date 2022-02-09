1535bdc5a0688488437a56b5c201b78da5439525
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2022-02-09 09:36:10 +0700

    enable web3

--- molaris-project/src/App.js
+++ molaris-project/src/App.js
@@ -1,10 +1,18 @@
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useMoralis } from "react-moralis";
 import ResetPassword from "./views/auth/ResetPassword";
 import SignIn from "./views/auth/SignIn";
 import SignUp from "./views/auth/SignUp";
 import Dashboard from "./views/dashboard/Dashboard";
 
 function App() {
+  const { enableWeb3 } = useMoralis();
+
+  useEffect(() => {
+    enableWeb3();
+  }, [enableWeb3]);
+
   return (
     <div className="App">
       <BrowserRouter>
